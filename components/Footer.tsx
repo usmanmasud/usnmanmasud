08fc466aa3b5b489247cabd050e409c6a4cec44c
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-22 22:43:09 +0100

    ..

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:usmanmasud250@gmail.com">
+        <a href="mailto:usmanmasud260@gmail.com">
           <MagicButton
             title="Contact me"
             icon={<FaLocationArrow />}
